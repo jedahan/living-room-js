d600337a26dd1390a8066cc80056251f2b432be7
Jonathan Dahan <hi@jonathan.is>
2018-03-23 10:58:32 -0400

    subway hacking

--- build/room.browser.js
+++ build/room.browser.js
@@ -11,9 +11,6 @@ var global$1 = typeof global !== "undefined" ? global :
             typeof self !== "undefined" ? self :
             typeof window !== "undefined" ? window : {}
 
-// shim for using process in browser
-// based off https://github.com/defunctzombie/node-process/blob/master/browser.js
-
 function defaultSetTimout() {
     throw new Error('setTimeout has not been defined');
 }
@@ -246,7 +243,7 @@ function getEnv (key) {
 class Room {
   constructor (uri) {
     this.uri = uri || getEnv('LIVING_ROOM_URI') || 'http://localhost:3000';
-    this._sockets = new Map();
+    this._sockets = new Map([['_general', io.connect(this.uri)]]);
     this._data = null;
     this._endpoint = null;
   }
@@ -276,6 +273,18 @@ class Room {
     if (!(this._data || this._endpoint)) {
       throw new Error(`please set _data and _endpoint using assert(), retract(), or select()`)
     }
+    const socket = this._sockets.get('_general');
+
+    if (socket.connected && (
+          this._endpoint == 'assert'
+        ||this._endpoint == 'retract' )) {
+      socket.emit(this._endpoint, [this._data]);
+    } else {
+      return this._post()
+    }
+  }
+
+  _post () {
     const endpoint = this.uri + '/' + this._endpoint;
 
     const post = {
@@ -295,11 +304,11 @@ class Room {
   facts () {
     this._data = {};
     this._endpoint = 'facts';
-    return this
+    this._db();
   }
 
-  select (facts) {
-    this._data = {facts};
+  select (fact) {
+    this._data = {fact};
     this._endpoint = 'select';
     return this
   }
@@ -323,13 +332,9 @@ class Room {
   }
 
   assert (fact) {
-    const socket = io.connect(this.uri);
-    socket.emit('assert', [fact]);
-    /*
-    this._data = {fact}
-    this._endpoint = 'assert'
-    this._db()
-    */
+    this._data = {fact};
+    this._endpoint = 'assert';
+    this._db();
     return this
   }
 
@@ -339,14 +344,6 @@ class Room {
     this._db();
     return this
   }
-
-/*
-  async retractEverythingAbout (name) {
-    this._data = {name}
-    this._endpoint = 'retractEverythingAbout'
-    await this._db()
-  }
-  */
 }
 
 return Room;
