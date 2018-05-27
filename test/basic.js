a07502e0f5c37f56fcb5927253130f7f4ee41e06
Jonathan Dahan <hi@jonathan.is>
2018-05-27 14:35:01 -0400

    wip maximal test case

--- test/basic.js
+++ test/basic.js
@@ -1,49 +1,76 @@
+const test = require(`ava`)
+const fetch = require(`node-fetch`)
 const { LivingRoomService } = require(`@living-room/service-js`)
 const Room = require(`../build/room`)
-const test = require(`ava`)
 
 test.beforeEach(async t => {
-  t.context.service = new LivingRoomService()
-  await t.context.service.listen({verbose: false})
-  t.context.room = new Room(`http://localhost:${t.context.service.port}`)
+  const service = new LivingRoomService({verbose: true})
+  const { port } = await service.listen()
+  t.context.room = new Room(`http://localhost:${port}`)
+  t.context.assert = async assert => {
+    const opts = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({facts: [{assert}]})
+    }
+    console.dir(`asserting to ${t.context.room._host}`)
+    return fetch(`${t.context.room._host}/messages`, opts).then(response => response.json())
+  }
 })
 
-test.afterEach(async t => {
-  t.context.service.close()
+test.afterEach.always(async t => {
+  const { room } = t.context
+  const opts = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  }
+  const facts = await fetch(`${room._host}/facts`, opts).then(response => response.json())
+  t.is(facts.assertions.length, 1)
 })
 
+// this leaks context to other tests...
 test(`await assert`, async t => {
+  t.plan(1)
   const { room } = t.context
-  const { facts } = await room.assert(`hello`)
+  const { facts } = await t.context.assert(`hello`)
   t.deepEqual(facts, [{assert: `hello`}])
-  const result = await room.select(`$word`)
-  t.deepEqual(result, [{
-    word: { word: `hello` }
-  }])
-})
-
-test.cb(`no callback subscribe`, t => {
-  const { room } = t.context
-  room.subscribe(`$what callback assert`, ({assertions, retractions}) => {
-    t.deepEqual(assertions, [{what: `no`}])
-    t.deepEqual(retractions, [])
-    t.end()
+  room.select(`$word`).then(result => {
+    t.deepEqual(result, [{
+      word: { word: `hello` }
+    }])
   })
-
-  room.assert(`no callback assert`).then()
 })
 
-test.cb(`no callback assert`, t => {
-  const { room } = t.context
-  room.on(`$what callback assert`, ({what}) => {
-    t.is(what, `no`)
-    t.end()
+test(`no callback subscribe`, t => {
+  t.plan(2)
+  return new Promise((resolve, reject) => {
+    const { room } = t.context
+    room.subscribe(`$what callback assert`, ({assertions, retractions}) => {
+      t.deepEqual(assertions, [{what: `no`}])
+      t.deepEqual(retractions, [])
+      resolve()
+    })
+
+    t.context.assert(`no callback assert`)
   })
-
-  room.assert(`no callback assert`).then()
 })
 
-test.cb(`multiple asserts`, t => {
+/*
+test(`no callback assert`, t => {
+  t.plan(1)
+  const { room } = t.context
+  return new Promise((resolve, reject) => {
+    room.on(`$what callback assert`, ({what}) => {
+      t.is(what, `no`)
+      resolve()
+    })
+
+    room.assert(`no callback assert`)
+  })
+})
+
+test(`multiple asserts`, t => {
+  t.plan(5)
   const { room } = t.context
   const animal = new Set([
     `party`,
@@ -56,25 +83,27 @@ test.cb(`multiple asserts`, t => {
   const asserts = Array.from(animal.values())
     .map(what => ({ assert: `animal ${what}` }))
 
-  room.on(`animal $what`, ({what}) => {
-    t.true(animal.delete(what))
-    if (animal.size === 0) t.end()
-  })
+  return new Promise((resolve, reject) => {
+    room.on(`animal $what`, ({what}) => {
+      t.true(animal.delete(what))
+      if (animal.size === 0) resolve()
+    })
 
-  room
-     .assert(`animal party`)
-     .assert(`animal car`)
-     .assert(`animal animal`)
+    room
+       .assert(`animal party`)
+       .assert(`animal car`)
+       .assert(`animal animal`)
 
-  room
-     .assert(`animal blue`)
-     .assert(`animal me`).then()
+    room
+       .assert(`animal blue`)
+       .assert(`animal me`)
+   })
 })
 
-/*
-test.cb(`once only gets called for existing assertions`, t => {
+test(`once only gets called for existing assertions`, t => {
   const { room } = t.context
   const asserts = new Set([ `first`, `second` ])
+  t.plan(2)
   let times = false
 
   const number = ({number}) => {
@@ -89,14 +118,14 @@ test.cb(`once only gets called for existing assertions`, t => {
     .assert(`first`)
     .assert(`second`)
 
-  setTimeout(() => {
-    room.assert(`third`).then(() => t.end())
-  }, 150)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      room.assert(`third`).then(resolve)
+    })
+  })
 })
-*/
 
-/*
-test.cb(`called for all assertions`, t => {
+test(`called for all assertions`, t => {
   const { room } = t.context
   let asserts = new Set([ `first`, `second`, `third` ])
   t.plan(asserts.size)
@@ -111,19 +140,19 @@ test.cb(`called for all assertions`, t => {
     .assert(`first`)
     .assert(`second`)
 
-  setTimeout(() => {
-    room.assert(`third`)
-      .then(() => {
-        t.is(asserts.size, 0)
-        //room.off(`$what`, what)
-        t.end()
-      })
-  }, 150)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      room.assert(`third`)
+        .then(() => {
+          t.is(asserts.size, 0)
+          //room.off(`$what`, what)
+          resolve()
+        })
+    }, 150)
+  })
 })
-*/
 
-/*
-test.cb('setImmediate clears calls a second time', t => {
+test('setImmediate clears calls a second time', t => {
   const { room } = t.context
   let times = 0
   const processed = response => {
@@ -143,13 +172,15 @@ test.cb('setImmediate clears calls a second time', t => {
     .assert('cool')
     .then()
 
-  room
-    .assert('like')
-    .assert('the')
-    .assert('coolest')
-    .then(response=> {
-      t.is(response, {facts: [{assert: `like`}, {assert: `the`}, {assert: `coolest`}]})
-      t.end()
-    })
+  return new Promise((resolve, reject) => {
+    room
+      .assert('like')
+      .assert('the')
+      .assert('coolest')
+      .then(response=> {
+        t.is(response, {facts: [{assert: `like`}, {assert: `the`}, {assert: `coolest`}]})
+        resolve()
+      })
+  })
 })
 */
